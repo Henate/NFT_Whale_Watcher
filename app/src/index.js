ee112f2d772d08af514df7ef00526d3b9808e93c
Henate <Henate@126.com>
2022-08-10 20:13:35 +0800

    Add frondend Render

--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { MoralisProvider } from "react-moralis";
 import { BrowserRouter } from "react-router-dom";
 
-
-
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="" serverUrl="">
+    <MoralisProvider
+      appId="DnnTLI7V7MIGLE6ZdfeXvocDjegVGecmH6pxczq2"
+      serverUrl="https://8tinxmhoislf.usemoralis.com:2053/server"
+    >
       <BrowserRouter>
-          <App />
+        <App />
       </BrowserRouter>
     </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
