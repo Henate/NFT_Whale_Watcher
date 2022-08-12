5f6335144d5d770b4b608e512b7865f6e07d1cf7
Henate <Henate@126.com>
2022-08-13 00:22:02 +0800

    update collection render

--- app/src/pages/Collection.js
+++ app/src/pages/Collection.js
@@ -1,15 +1,50 @@
 import React from "react";
 import { Table } from "antd";
-import { useState} from "react";
+import { useState, useEffect } from "react";
 import { Icon } from "web3uikit";
 import etherscan from "../images/etherscan.png";
 import opensea from "../images/opensea.png";
+import { useNavigate, useParams } from "react-router-dom";
+import { allCollections } from "../collectionsFile";
+import axios from "axios";
 
 function Collection() {
   const [data, setData] = useState();
+  const [collectionData, setCollectionData] = useState();
   const [largest, setLargest] = useState("NA");
   const [highBuy, setHighBuy] = useState("NA");
   const [longHold, setLongHold] = useState("NA");
+  const { collection } = useParams();
+
+  useEffect(() => {
+    const result = allCollections.filter((obj) => {
+      return obj.slug === collection;
+    });
+
+    setCollectionData(result[0]);
+
+    async function getCol() {
+      const res = await axios.get("http://localhost:4000/collection", {
+        params: { slug: collection },
+      });
+
+      const data = res.data;
+      const dataArray = Object.values(data);
+      setData(dataArray);
+
+      const bags = dataArray.map((a) => a.amount);
+      const holds = dataArray.map((a) => a.avgHold);
+      const prices = dataArray.map((a) => a.avgPrice);
+      const highestAmount = Math.max(...bags);
+      const longestHold = Math.max(...holds);
+      const highestBuy = Math.max(...prices);
+      setLargest(highestAmount);
+      setHighBuy(highestBuy.toFixed(2));
+      setLongHold(Math.floor(longestHold));
+    }
+
+    getCol();
+  }, [collection]);
 
   const columns = [
     {
@@ -30,14 +65,19 @@ function Collection() {
       dataIndex: "avgPrice",
     },
     {
-      title:"Quantity Change",
+      title: "Quantity Change",
       dataIndex: "recentTx",
     },
   ];
-  
 
   return (
     <>
+      {collectionData && (
+        <div className="title">
+          <img src={collectionData.img} alt="colLogo" className="logoImg" />
+          {collectionData.name}
+        </div>
+      )}
       <div className="stats">
         <div className="colStats">
           <div>
